981bde130bff554fe6460e56fec90b82a4552a7a
rajanisar288 <raaji4194@gmail.com>
2024-11-12 19:00:41 +0500

    fix git issue

--- README.md
+++ README.md
@@ -1,27 +1,39 @@
-# CiphersLab
+# Ciphers Lab - Dynamic Angular Form Application
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.0.
 
-## Development server
+## Prerequisites
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+To run this project, you need to have the following versions installed on your system:
 
-## Code scaffolding
+- **Node.js**: Version 18.13.0 or higher
+- **Angular CLI**: Version 17.0.0 or higher
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+### Step 1: Install Node.js
 
+1. **Download and Install Node.js**  
+   If you don't have Node.js installed, download it from [https://nodejs.org/](https://nodejs.org/) and install it. Make sure the version is 18.13.0 or higher.
+
+2. **Verify Node.js Installation**  
+   Open a terminal and run:
+   ```bash
+   node -v
+3. **Setup the project** 
+
+    Go to project directory
+
+   Open a terminal and run the following command:
+   ```bash
+    npm install
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+## Runn the app
 
-## Running end-to-end tests
+Run `ng serve` to run the project. 
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+Go to the browser and paste this url http://localhost:4200/
 
-## Further help
 
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+interface FormType {
+  type: string;
+  label: string;
+  placeholder?: string;
+  name: string;
+  validation?: {
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    pattern?: string;
+  };
+  options?: Array<{ label: string; value: string }>;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,
+    ReactiveFormsModule,
+    HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ciphers_lab';
+  form: FormGroup = new FormGroup({});
+  formConfiguration: any;
+  loader:boolean = true;
+  constructor(private _formBuilder: FormBuilder, private _http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForm();
+  }
+  getForm() {
+    this._http.get('/assets/form-config.json').subscribe((result: any) => {
+      this.formConfiguration = result;
+      this.createForm();
+    });
+  }
+  createForm() {
+    const controlsConfig: { [key: string]: any } = {};
+    this.formConfiguration.fields.forEach( (field: FormType)=> {
+      const validators = [];
+      if (field.validation) {
+        if (field.validation.required) validators.push(Validators.required);
+        if (field.validation.minLength) validators.push(Validators.minLength(field.validation.minLength));
+        if (field.validation.maxLength) validators.push(Validators.maxLength(field.validation.maxLength));
+        if (field.validation.pattern) validators.push(Validators.pattern(field.validation.pattern));
+      }
+      controlsConfig[field.name] = this._formBuilder.control('', validators);
+    });
+    this.form = this._formBuilder.group(controlsConfig);
+    this.loader = false
+  }
+
+  onSubmit() {
+    if (this.form && this.form.valid) {
+        alert('Form successfully submitted')
+        this.form.reset();
+    } else {
+      alert('Form has errors.')
+      console.log("Form has errors.");
+    }
+  }
 }
